0a70949e70604df99d0bcba5c13dc2bb1267df75
binyamyohannes <olangobinyam@gmail.com>
2024-03-06 11:32:29 +0300

    Update user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :set_user, only: %i[destroy]
+  before_action :set_user, only: %i[show destroy]
 
   def index
     @users = User.all
