5e21ea869df9a6059b41ada230607e41e8c76c89
binyamyohannes <olangobinyam@gmail.com>
2024-03-12 16:29:45 +0300

    Add reservation

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -28,6 +28,6 @@ class Api::V1::ReservationsController < ApplicationController
   private
 
   def reservation_params
-    params.require(:reservation).permit(:date_of_reservation)
+    params.require(:reservation).permit(:date_of_reservation, :user_id, :doctor_id)
   end
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,5 +2,7 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :doctor
 
+  validates :user, presence: true
+  validates :doctor, presence: true
   validates :date_of_reservation, presence: true
 end
